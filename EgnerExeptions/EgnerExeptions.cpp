e0309c090d8c08587a499b566a6d5462491ba0aa
Deagle <robby00518@gmail.com>
2024-04-16 10:56:12 -0400

    Add project files.

---
+++ EgnerExeptions/EgnerExeptions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <exception>
+
+using namespace std;
+char character(char,int);
+int main() {
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+char character(char start, int offset){
+
+};
